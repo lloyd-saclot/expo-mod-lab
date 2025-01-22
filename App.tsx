232140d9bc91a553d68a30ea4be950265137b76b
Lloyd-Saclot <164666621+lloyd-saclot@users.noreply.github.com>
2025-01-22 09:41:24 -0700

    Added icons and middle message

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+
+import iconImage from './assets/box.png';
+import messageImage from './assets/speech-bubble.png';
 
 export default function App() {
   return (
@@ -16,8 +19,22 @@ export default function App() {
       <Text style={styles.message}>Messages</Text>
 
 
+      <TouchableOpacity style={styles.iconButton}>
+        <View style={styles.iconBackground}>
+          <Image source={iconImage} style={{ width: 20, height: 20 }} />
+        </View>
+      </TouchableOpacity>
+      
+
+      <View style={styles.messageiconBackground}>
+          <Image source={messageImage} style={{ width: 70, height: 70 }} />
+      </View>
+
+      <Text style={styles.middleTitle}>No Messages</Text>
+      <Text style={styles.middleText}> When you have messages, you'll see them here  </Text>
 
 
+      <Text></Text>
 
 
 
@@ -44,4 +61,41 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 30,
   },
+
+  middleTitle: {
+    fontWeight: 'bold',
+    fontSize: 22,
+    paddingTop: 210,
+  },
+
+  middleText: {
+    paddingTop: 10,
+    fontSize: 17,
+  },
+
+  iconButton: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    paddingTop: 80,
+    paddingRight: 30,
+  },
+
+  iconBackground: {
+    backgroundColor: '#f2f0f0',
+    borderRadius: 50,
+    width: 40,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  messageiconBackground: {
+    backgroundColor: '#f2f0f0',
+    borderRadius: 150,
+    width: 150,
+    height: 150,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
